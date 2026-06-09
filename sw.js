32f1238b5665ed5e912518d3032f25d9b95d6533
flowdeepwithin <flowdeepwithin@gmail.com>
2026-06-09 11:48:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
-self.addEventListener('install', event => { self.skipWaiting(); });
-self.addEventListener('activate', event => { event.waitUntil((async()=>{ const keys=await caches.keys(); await Promise.all(keys.map(k=>caches.delete(k))); await self.clients.claim(); })()); });
-self.addEventListener('fetch', event => { return; });
+self.addEventListener('install', event => self.skipWaiting());
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
