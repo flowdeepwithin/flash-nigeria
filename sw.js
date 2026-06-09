b15c0049ccd5683501841c190235e70dc2e784fa
flowdeepwithin <flowdeepwithin@gmail.com>
2026-06-09 14:40:28 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-// Flash Nigeria SW v6 - no stale cache
-self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', event => { event.respondWith(fetch(event.request)); });
